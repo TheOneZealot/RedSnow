95dbd51627443f0393e79bd75422630e451574e8
Henrik Melsom <henrik@melsom.se>
2016-05-29 17:39:43 +0200

    formatted main

--- src/Main.hx
+++ src/Main.hx
@@ -1,34 +1,33 @@
-
 import luxe.GameConfig;
 import luxe.Input;
 
-class Main extends luxe.Game {
-
-    override function config(config:GameConfig) {
-
+class Main extends luxe.Game
+{
+    override function config(config:GameConfig)
+	{
         config.window.title = 'luxe game';
         config.window.width = 960;
         config.window.height = 640;
         config.window.fullscreen = false;
 
         return config;
+    }
 
-    } //config
+    override function ready()
+	{
 
-    override function ready() {
+    }
 
-    } //ready
-
-    override function onkeyup( e:KeyEvent ) {
-
-        if(e.keycode == Key.escape) {
+    override function onkeyup(e:KeyEvent)
+	{
+        if(e.keycode == Key.escape)
+		{
             Luxe.shutdown();
         }
+    }
 
-    } //onkeyup
+    override function update(dt:Float)
+	{
 
-    override function update(dt:Float) {
-
-    } //update
-
-} //Main
+    }
+}
